d0762dced849cda8668ee959d57f0dcdb8bab0ca
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2024-12-10 10:13:15 +1100

    Replace single logo

--- README.md
+++ README.md
@@ -5,24 +5,9 @@
 !-- --- -->
 
 # Ninja — Neural Information Network of Joint Agents
-<p align="center"><table>
-<tr>
-<td><img src="./docs/logos/logo_01.jpg" width="100px"></td><td><img src="./docs/logos/logo_02.jpg" width="100px"></td><td><img src="./docs/logos/logo_03.jpg" width="100px"></td><td><img src="./docs/logos/logo_04.jpg" width="100px"></td><td><img src="./docs/logos/logo_05.jpg" width="100px"></td><td><img src="./docs/logos/logo_06.jpg" width="100px"></td><td><img src="./docs/logos/logo_07.jpg" width="100px"></td><td><img src="./docs/logos/logo_08.jpg" width="100px"></td></tr>
-<tr>
-<td><img src="./docs/logos/logo_09.jpg" width="100px"></td><td><img src="./docs/logos/logo_10.jpg" width="100px"></td><td><img src="./docs/logos/logo_11.jpg" width="100px"></td><td><img src="./docs/logos/logo_12.jpg" width="100px"></td><td><img src="./docs/logos/logo_13.jpg" width="100px"></td><td><img src="./docs/logos/logo_14.jpg" width="100px"></td><td><img src="./docs/logos/logo_15.jpg" width="100px"></td><td><img src="./docs/logos/logo_16.jpg" width="100px"></td></tr>
-<tr>
-<td><img src="./docs/logos/logo_17.jpg" width="100px"></td><td><img src="./docs/logos/logo_18.jpg" width="100px"></td><td><img src="./docs/logos/logo_19.jpg" width="100px"></td><td><img src="./docs/logos/logo_20.jpg" width="100px"></td><td><img src="./docs/logos/logo_21.jpg" width="100px"></td><td><img src="./docs/logos/logo_22.jpg" width="100px"></td><td><img src="./docs/logos/logo_23.jpg" width="100px"></td><td><img src="./docs/logos/logo_24.jpg" width="100px"></td></tr>
-<tr>
-<td><img src="./docs/logos/logo_25.jpg" width="100px"></td><td><img src="./docs/logos/logo_26.jpg" width="100px"></td><td><img src="./docs/logos/logo_27.jpg" width="100px"></td><td><img src="./docs/logos/logo_28.jpg" width="100px"></td><td><img src="./docs/logos/logo_29.jpg" width="100px"></td><td><img src="./docs/logos/logo_30.jpg" width="100px"></td><td><img src="./docs/logos/logo_31.jpg" width="100px"></td><td><img src="./docs/logos/logo_32.jpg" width="100px"></td></tr>
-<tr>
-<td><img src="./docs/logos/logo_33.jpg" width="100px"></td><td><img src="./docs/logos/logo_34.jpg" width="100px"></td><td><img src="./docs/logos/logo_35.jpg" width="100px"></td><td><img src="./docs/logos/logo_36.jpg" width="100px"></td><td><img src="./docs/logos/logo_37.jpg" width="100px"></td><td><img src="./docs/logos/logo_38.jpg" width="100px"></td><td><img src="./docs/logos/logo_39.jpg" width="100px"></td><td><img src="./docs/logos/logo_40.jpg" width="100px"></td></tr>
-<tr>
-<td><img src="./docs/logos/logo_41.jpg" width="100px"></td><td><img src="./docs/logos/logo_42.jpg" width="100px"></td><td><img src="./docs/logos/logo_43.jpg" width="100px"></td><td><img src="./docs/logos/logo_44.jpg" width="100px"></td><td><img src="./docs/logos/logo_45.jpg" width="100px"></td><td><img src="./docs/logos/logo_46.jpg" width="100px"></td><td><img src="./docs/logos/logo_47.jpg" width="100px"></td><td><img src="./docs/logos/logo_48.jpg" width="100px"></td></tr>
-<tr>
-<td><img src="./docs/logos/logo_49.jpg" width="100px"></td><td><img src="./docs/logos/logo_50.jpg" width="100px"></td><td><img src="./docs/logos/logo_51.jpg" width="100px"></td><td><img src="./docs/logos/logo_52.jpg" width="100px"></td><td><img src="./docs/logos/logo_53.jpg" width="100px"></td><td><img src="./docs/logos/logo_54.jpg" width="100px"></td><td><img src="./docs/logos/logo_55.jpg" width="100px"></td><td><img src="./docs/logos/logo_56.jpg" width="100px"></td></tr>
-<tr>
-<td><img src="./docs/logos/logo_57.jpg" width="100px"></td><td><img src="./docs/logos/logo_58.jpg" width="100px"></td><td><img src="./docs/logos/logo_59.jpg" width="100px"></td><td><img src="./docs/logos/logo_60.jpg" width="100px"></td><td><img src="./docs/logos/logo_61.jpg" width="100px"></td><td><img src="./docs/logos/logo_62.jpg" width="100px"></td><td><img src="./docs/logos/logo_63.jpg" width="100px"></td><td><img src="./docs/logos/logo_64.jpg" width="100px"></td></tr>
-</table></p>
+<p align="center">
+    <img src="./docs/logos/logo_55.jpg" width="100px">
+</p>
 
 
 THIS REPOSITORY IS CURRENTLY UNDER ACTIVE DEVELOPMENT
--- docs/logos/gen_grid.sh
+++ docs/logos/gen_grid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Time-stamp: "2024-12-10 10:06:29 (ywatanabe)"
-# File: ./ninja/docs/logos/gen_tile.sh
+# Time-stamp: "2024-12-10 10:10:07 (ywatanabe)"
+# File: ./ninja/docs/logos/gen_grid.sh
 
 echo '<p align="center"><table>' > grid.html
 for ((i=0; i<8; i++)); do
@@ -13,5 +13,24 @@ for ((i=0; i<8; i++)); do
 done
 echo '</table></p>' >> grid.html
 
+
+# <!-- <p align="center"><table>
+#  !-- <tr>
+#  !-- <td><img src="./docs/logos/logo_01.jpg" width="100px"></td><td><img src="./docs/logos/logo_02.jpg" width="100px"></td><td><img src="./docs/logos/logo_03.jpg" width="100px"></td><td><img src="./docs/logos/logo_04.jpg" width="100px"></td><td><img src="./docs/logos/logo_05.jpg" width="100px"></td><td><img src="./docs/logos/logo_06.jpg" width="100px"></td><td><img src="./docs/logos/logo_07.jpg" width="100px"></td><td><img src="./docs/logos/logo_08.jpg" width="100px"></td></tr>
+#  !-- <tr>
+#  !-- <td><img src="./docs/logos/logo_09.jpg" width="100px"></td><td><img src="./docs/logos/logo_10.jpg" width="100px"></td><td><img src="./docs/logos/logo_11.jpg" width="100px"></td><td><img src="./docs/logos/logo_12.jpg" width="100px"></td><td><img src="./docs/logos/logo_13.jpg" width="100px"></td><td><img src="./docs/logos/logo_14.jpg" width="100px"></td><td><img src="./docs/logos/logo_15.jpg" width="100px"></td><td><img src="./docs/logos/logo_16.jpg" width="100px"></td></tr>
+#  !-- <tr>
+#  !-- <td><img src="./docs/logos/logo_17.jpg" width="100px"></td><td><img src="./docs/logos/logo_18.jpg" width="100px"></td><td><img src="./docs/logos/logo_19.jpg" width="100px"></td><td><img src="./docs/logos/logo_20.jpg" width="100px"></td><td><img src="./docs/logos/logo_21.jpg" width="100px"></td><td><img src="./docs/logos/logo_22.jpg" width="100px"></td><td><img src="./docs/logos/logo_23.jpg" width="100px"></td><td><img src="./docs/logos/logo_24.jpg" width="100px"></td></tr>
+#  !-- <tr>
+#  !-- <td><img src="./docs/logos/logo_25.jpg" width="100px"></td><td><img src="./docs/logos/logo_26.jpg" width="100px"></td><td><img src="./docs/logos/logo_27.jpg" width="100px"></td><td><img src="./docs/logos/logo_28.jpg" width="100px"></td><td><img src="./docs/logos/logo_29.jpg" width="100px"></td><td><img src="./docs/logos/logo_30.jpg" width="100px"></td><td><img src="./docs/logos/logo_31.jpg" width="100px"></td><td><img src="./docs/logos/logo_32.jpg" width="100px"></td></tr>
+#  !-- <tr>
+#  !-- <td><img src="./docs/logos/logo_33.jpg" width="100px"></td><td><img src="./docs/logos/logo_34.jpg" width="100px"></td><td><img src="./docs/logos/logo_35.jpg" width="100px"></td><td><img src="./docs/logos/logo_36.jpg" width="100px"></td><td><img src="./docs/logos/logo_37.jpg" width="100px"></td><td><img src="./docs/logos/logo_38.jpg" width="100px"></td><td><img src="./docs/logos/logo_39.jpg" width="100px"></td><td><img src="./docs/logos/logo_40.jpg" width="100px"></td></tr>
+#  !-- <tr>
+#  !-- <td><img src="./docs/logos/logo_41.jpg" width="100px"></td><td><img src="./docs/logos/logo_42.jpg" width="100px"></td><td><img src="./docs/logos/logo_43.jpg" width="100px"></td><td><img src="./docs/logos/logo_44.jpg" width="100px"></td><td><img src="./docs/logos/logo_45.jpg" width="100px"></td><td><img src="./docs/logos/logo_46.jpg" width="100px"></td><td><img src="./docs/logos/logo_47.jpg" width="100px"></td><td><img src="./docs/logos/logo_48.jpg" width="100px"></td></tr>
+#  !-- <tr>
+#  !-- <td><img src="./docs/logos/logo_49.jpg" width="100px"></td><td><img src="./docs/logos/logo_50.jpg" width="100px"></td><td><img src="./docs/logos/logo_51.jpg" width="100px"></td><td><img src="./docs/logos/logo_52.jpg" width="100px"></td><td><img src="./docs/logos/logo_53.jpg" width="100px"></td><td><img src="./docs/logos/logo_54.jpg" width="100px"></td><td><img src="./docs/logos/logo_55.jpg" width="100px"></td><td><img src="./docs/logos/logo_56.jpg" width="100px"></td></tr>
+#  !-- <tr>
+#  !-- <td><img src="./docs/logos/logo_57.jpg" width="100px"></td><td><img src="./docs/logos/logo_58.jpg" width="100px"></td><td><img src="./docs/logos/logo_59.jpg" width="100px"></td><td><img src="./docs/logos/logo_60.jpg" width="100px"></td><td><img src="./docs/logos/logo_61.jpg" width="100px"></td><td><img src="./docs/logos/logo_62.jpg" width="100px"></td><td><img src="./docs/logos/logo_63.jpg" width="100px"></td><td><img src="./docs/logos/logo_64.jpg" width="100px"></td></tr>
+#  !-- </table></p> -->
 
 # EOF
