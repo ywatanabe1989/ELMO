effd8339909f690f43318926c478ac40d83a31a7
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-01-02 18:18:27 +1100

    Refactor: Rename functions, use python env path for mdjson, and remove unused code.

--- llemacs.el/04-llemacs-cvt/mdjson.sh
+++ llemacs.el/04-llemacs-cvt/mdjson.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Time-stamp: "2025-01-02 04:04:16 (ywatanabe)"
+# Time-stamp: "2025-01-02 11:30:39 (ywatanabe)"
 # File: /home/ywatanabe/.emacs.d/lisp/llemacs/workspace/resources/scripts/json2md.sh
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -40,21 +40,8 @@ main() {
 
     # Process each input file
     for input in "$@"; do
-        source /home/ywatanabe/.env/bin/activate
+        # source /home/ywatanabe/.env/bin/activate
         mdjson "$input"
-        # local input_extension="${input##*.}"
-        # local output
-
-        # if [[ "$input_extension" == "json" ]]; then
-        #     output="${input%.json}.md"
-        #     mdjson "$input" # -o "$output"            
-        # elif [[ "$input_extension" == "md" ]]; then
-        #     output="${input%.md}.json"
-        #     mdjson "$input" # -o "$output"                        
-        # else
-        #     echo "Error: Unsupported file extension: $input_extension" >&2
-        #     continue
-        # fi
     done
 }
 
---
+++ llemacs.el/04-llemacs-cvt/test.md
@@ -0,0 +1,7 @@
+<!-- ---
+!-- title: 2025-01-02 11:35:17
+!-- author: ywata-note-win
+!-- date: /home/ywatanabe/proj/llemacs/llemacs.el/04-llemacs-cvt/test.md
+!-- --- -->
+
+# This is a test markdown file
